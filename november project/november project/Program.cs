fdfe4f4f68e61634a38b0ee9ed3e7d02f7e0d48b
Pookia <54947020+PookiaPie@users.noreply.github.com>
2019-10-23 14:36:15 +0200

    Vad jag ska göra för sepl

--- november project/november project/Program.cs
+++ november project/november project/Program.cs
@@ -10,6 +10,10 @@ namespace november_project
     {
         static void Main(string[] args)
         {
+            //Ett matlagningsspel där man har en viss summa att spendera, och ska välja ingredienser att lägga i en gryta. Sedan finns det en jury som gillar olika ingredienser.//
+
+
+
         }
     }
 }
